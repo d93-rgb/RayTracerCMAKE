6271330311f5db3bd54760cc68a027d1f6443e59
ggfaEM <damian-93@hotmail.de>
2019-03-30 19:59:00 +0100

    Preparing gui

--- RayTracer/include/gui/gui.h
+++ RayTracer/include/gui/gui.h
@@ -8,11 +8,28 @@ struct Gui
 	
 	void init()
 	{
+        if( !glfwInit() )
+        {
+            std::cerr << "GLFW initialization failed" << std::endl;
+            exit(1);
+        }
+
+        if ( !(GLFWwindow *win = glfwOpenWindow(
+                        640, 480, "Ray Tracer", 
+                        nullptr, nullptr) ))
+        {
+            std::cerr << "Error: GLFW window could not be opened." 
+                << std::endl;
+            glfwTerminate();
+            exit(1);
+        }
+
+        glfwEnable()
 		TwInit(TW_OPENGL, nullptr);
 
 		TwWindowSize(500, 250);
 
-		bar = TwNewBar("MyBar");
+		bar = TwNewBar("RT TweakBar");
 
 		TwDraw();
 	}
