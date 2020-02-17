440dd09a27a0a4bcb9c0bd74e88b9cc10eff6e4f
d93-rgb <ggfa1993@gmail.com>
2020-02-17 21:20:31 +0100

    added glfw

--- RayTracer/src/main.cpp
+++ RayTracer/src/main.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <set>
 
+#include <GLFW/glfw3.h>
+
 #include "core/rt.h"
 #include "core/renderer.h"
 #include "samplers/sampler2D.h"
@@ -196,7 +198,135 @@ LRESULT CALLBACK WindowProc(
 
 int main(int argc, char* argv[])
 {
+	int error_code;
+	const char* error_description;
 
+	if (glfwInit() == GLFW_FALSE)
+	{
+		error_code = glfwGetError(&error_description);
+
+		std::cout << error_description << std::endl;
+		std::exit(1);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(
+		screenWidth,
+		screenHeight,
+		"Minimal OpenGL Example",
+		nullptr,
+		nullptr);
+
+	if (window == nullptr) {
+		error_code = glfwGetError(&error_description);
+
+		std::cout << error_description << std::endl;
+		glfwTerminate();
+		std::exit(1);
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+	glfwSetKeyCallback(window, key_callback);
+
+	//glewExperimental = GL_TRUE;
+
+	if ((error_code = glewInit()) != GLEW_OK) {
+		std::cout << glewGetErrorString(error_code) << std::endl;
+		std::exit(1);
+	}
+
+	std::string file_path = std::string(__FILE__);
+	file_path = file_path.substr(0, file_path.find_last_of("\\/"));
+
+	std::string vertexPath = file_path + "/shaders/vertex_shader.glsl";
+	std::string fragPath = file_path + "/shaders/fragment_shader.glsl";
+
+	std::string vertexCode;
+	std::string fragmentCode;
+
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+
+	// ensure ifstream objects can throw exceptions:
+	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try
+	{
+		// open files
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragPath);
+		std::stringstream vShaderStream, fShaderStream;
+		// read file's buffer contents into streams
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+		// close file handlers
+		vShaderFile.close();
+		fShaderFile.close();
+		// convert stream into string
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "error: shader file(s) not successfully read" << std::endl;
+		std::exit(1);
+	}
+
+	const char* vShaderCode = vertexCode.c_str();
+	const char* fShaderCode = fragmentCode.c_str();
+	unsigned int vertex, fragment;
+
+	// vertex shader
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vShaderCode, NULL);
+	glCompileShader(vertex);
+	checkCompileErrors(vertex, "VERTEX");
+
+	// fragment Shader
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fShaderCode, NULL);
+	glCompileShader(fragment);
+	checkCompileErrors(fragment, "FRAGMENT");
+
+	// shader Program
+	GLint ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	glLinkProgram(ID);
+	checkCompileErrors(ID, "PROGRAM");
+
+	// delete the shaders as they're linked into our program now and no longer necessery
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+
+	glViewport(0, 0, screenWidth, screenHeight);
+	glEnable(GL_DEPTH_TEST);
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwWaitEvents();
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(ID);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+
+	return 0;
 }
 
 #endif
